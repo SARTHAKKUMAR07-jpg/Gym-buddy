4388534458084d71eb8ed0285a5727a890c70e29
SARTHAK KUMAR <sarthak009999@gmail.com>
2026-06-12 16:35:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector("button").addEventListener("click", function () {
+    alert("Welcome to Gym Buddy!");
+});
+document.querySelector("button").addEventListener("click", function () {
+    alert("Feature Coming Soon!");
+});
